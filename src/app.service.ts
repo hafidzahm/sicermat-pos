c2af31105e9548b908258757880745f14efc273f
Abdul <hafizhahm123@gmail.com>
2025-08-07 18:50:35 +0700

    refactor: refactoring console

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Global, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongoClient, Db, Collection } from 'mongodb';
+import bcrypt from 'bcryptjs';
 
 @Global()
 @Injectable()
@@ -9,7 +10,7 @@ export class AppService {
 
   async getDatabase(nameDatabase: string): Promise<Db> {
     const MONGO_URI = this.configService.get<string>('MONGO_URI');
-    console.log(MONGO_URI, '<----- ENV');
+    // console.log(MONGO_URI, '<----- ENV');
 
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     const client = new MongoClient(MONGO_URI as string);
@@ -18,11 +19,11 @@ export class AppService {
       // Send a ping to confirm a successful connection
       await client.db(nameDatabase).command({ ping: 1 });
       console.log(
-        `Pinged your deployment. You successfully connected to MongoDB! on db ${nameDatabase}`,
+        `You successfully connected to MongoDB! Currently on db ${nameDatabase}`,
       );
       return client.db(nameDatabase);
     } catch (error) {
-      console.log('db connection fail');
+      console.log(`db connection fail: ${error}`);
 
       await client.close();
       throw error;
@@ -32,5 +33,15 @@ export class AppService {
   async getCollectionDevelopment(nameCollection: string): Promise<Collection> {
     const database: Db = await this.getDatabase('SiCermat-DB');
     return database.collection(nameCollection);
+  }
+
+  hashPassword(plainPassword: string) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(plainPassword, salt);
+    return hash;
+  }
+
+  comparePassword(plainPassword: string, hashedPassword: string) {
+    return bcrypt.compareSync(plainPassword, hashedPassword);
   }
 }
