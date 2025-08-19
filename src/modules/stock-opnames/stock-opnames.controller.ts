163685b5af178a20f70ea8995f25137954f6b587
Abdul <hafizhahm123@gmail.com>
2025-08-19 14:36:08 +0700

    module SO: crud

--- src/modules/stock-opnames/stock-opnames.controller.ts
+++ src/modules/stock-opnames/stock-opnames.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Logger, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+} from '@nestjs/common';
 import { StockOpnamesService } from './stock-opnames.service';
 import { ZodValidationPipe } from 'src/common/pipes/pipes';
 import { stockOpnameSchema } from './schema/stock-opnames.schema';
@@ -7,7 +17,7 @@ import type { StockOpnameDto } from './schema/stock-opnames.schema';
 @Controller('/api/stock-opnames')
 export class StockOpnamesController {
   constructor(private readonly libs: StockOpnamesService) {}
-
+  // gabung ke group so
   @Post()
   @UsePipes(new ZodValidationPipe(stockOpnameSchema))
   async createNewStockOpname(@Body() item: StockOpnameDto) {
@@ -15,7 +25,45 @@ export class StockOpnamesController {
     const status = await this.libs.addItem(item);
     return {
       status,
-      message: 'new stock opname successfully added!',
+      message: 'new stock opname successfully included to user/ group!',
+    };
+  }
+
+  @Get()
+  async getAllStockOpname() {
+    const result = await this.libs.getAllItem();
+    return {
+      result,
+    };
+  }
+
+  @Get(':id')
+  async getStockOpnameById(@Param('id') id: string) {
+    const result = await this.libs.getItemById(id);
+    return {
+      result: result.item,
+      message: 'Stock opname with inputed id found',
+    };
+  }
+
+  @Delete(':id')
+  async deleteStockOpnameById(@Param('id') id: string) {
+    const result = await this.libs.deleteItemById(id);
+    return {
+      status: result?.acknowledged,
+      message: 'Stock opname removed from group successfully',
+    };
+  }
+
+  @Put(':id')
+  async changeStockOpname(
+    @Param('id') id: string,
+    @Body() body: StockOpnameDto,
+  ) {
+    const result = await this.libs.changeDataItem(id, body);
+    return {
+      status: result?.acknowledged,
+      message: 'Stock opname changed successfully',
     };
   }
 }
--- src/modules/stock-opnames/stock-opnames.service.ts
+++ src/modules/stock-opnames/stock-opnames.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { DatabaseService } from 'src/common/helpers/database/database.service';
 import { StockOpnameDto } from './schema/stock-opnames.schema';
@@ -22,6 +23,8 @@ export class StockOpnamesService {
       GroupStockOpnameId:
         typeof item.GroupStockOpnameId === 'string' &&
         new ObjectId(item.GroupStockOpnameId),
+      startedAt: new Date().toISOString(),
+      endedAt: '',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString(),
     };
@@ -33,5 +36,60 @@ export class StockOpnamesService {
       Logger.error(error, 'addItem StockOpnameService');
       throw new InternalServerErrorException('Unable to add StockOpname');
     }
+  }
+
+  async getItemById(id: string) {
+    const collection = await this.collection();
+    const findedItem = await collection.findOne<StockOpnameDto>({
+      _id: new ObjectId(id),
+    });
+
+    if (!findedItem) {
+      throw new NotFoundException('Stock opname with inputed id not found');
+    }
+
+    return { item: findedItem };
+  }
+
+  async deleteItemById(id: string) {
+    const collection = await this.collection();
+    const foundedItem = await this.getItemById(id);
+
+    if (foundedItem && foundedItem.item) {
+      const response = await collection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      return response;
+    }
+  }
+
+  async changeDataItem(id: string, body: StockOpnameDto) {
+    const collection = await this.collection();
+    const findedItem = await this.getItemById(id);
+
+    if (findedItem && findedItem.item) {
+      const changedData = {
+        UserId: new ObjectId(body.UserId),
+        GroupStockOpnameId: new ObjectId(body.GroupStockOpnameId),
+        endedAt: body.endedAt,
+        note: body.note,
+        updatedAt: new Date().toISOString(),
+      };
+
+      return collection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: changedData,
+        },
+      );
+    }
+  }
+
+  async getAllItem() {
+    const collection = await this.collection();
+    return await collection.find().toArray();
   }
 }
