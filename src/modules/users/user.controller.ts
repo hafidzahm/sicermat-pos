5ca99eeb1dbf6f6f9898e48dbea7042c21c48690
Abdul Hafizh <hafizhahm123@gmail.com>
2025-09-06 13:09:50 +0700

    chore: user role configuration

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -14,6 +14,7 @@ import { UserService } from './user.service';
 import type { CreateUserDto } from './schema/user.schema';
 import { createUserSchema } from './schema/user.schema';
 import { ZodValidationPipe } from 'src/common/pipes/pipes';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('/api/users')
 export class UserController {
@@ -22,6 +23,7 @@ export class UserController {
   @Post()
   @UsePipes(new ZodValidationPipe(createUserSchema))
   @HttpCode(201)
+  @Roles(['admin'])
   async registerNewUser(@Body() body: CreateUserDto) {
     try {
       const { acknowledged } = await this.libs.registerNewUser(body);
@@ -40,12 +42,14 @@ export class UserController {
   }
 
   @Get(':id')
+  @Roles(['admin'])
   async findUserById(@Param('id') id: string) {
     const result = await this.libs.findItemById(id);
     return result;
   }
 
   @Delete(':id')
+  @Roles(['admin'])
   async deleteUserById(@Param('id') id: string) {
     const result = await this.libs.deleteItemById(id);
     return {
@@ -56,6 +60,7 @@ export class UserController {
   }
 
   @Delete('/username/:usn')
+  @Roles(['admin'])
   async deleteUserByUsername(@Param('usn') usn: string) {
     const result = await this.libs.findUserByUsername(usn);
     return {
